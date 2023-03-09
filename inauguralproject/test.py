6495966a4550fa266865319dc2e4b7a7107c3f82
bpr871 <bpr871@alumni.ku.dk>
2023-03-09 16:19:34 +0100

    test

---
+++ inauguralproject/test.py
@@ -0,0 +1,16 @@
+
+def u_func(Q, rho, v, T_M, T_F, epsilon, C, ):
+    """ square numpy array
+    
+    Args:
+    
+        x (ndarray): input array
+        
+    Returns:
+    
+        y (ndarray): output array
+    
+    """
+    
+    y = x**2
+    return y
