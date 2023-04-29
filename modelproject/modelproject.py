a09a1b86ccbfcee598d1c052aea4f733c9fee1cb
bpr871 <bpr871@alumni.ku.dk>
2023-04-29 12:46:12 +0200

    inaugural update

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -20,3 +20,49 @@ def solve_ss(alpha, c):
     result = optimize.root_scalar(obj,bracket=[0.1,100],method='bisect')
     
     return result
+
+def demand(a, b, p_i, p_j):
+    """ Returns demand faced by firm i
+
+    Args:
+        p_i (float): price for firm i 
+        p_j (float): price for firm j
+
+    Returns:
+        demand (RootResults): the return is demand
+
+    """ 
+    
+    # a. Objective function
+    if p_i > p_j: 
+        demand = 0
+    elif p_i == p_j: 
+        demand = 1/2*(a-b*p_i)
+    else:
+        demand = (a-b*p_i)
+
+    return demand
+
+def profit(p_i, p_j, c, demand):
+    """ Returns profit for firm i
+
+    Args:
+        c (float): marginal costs
+        p_i (float): price for firm i 
+        p_j (float): price for firm j
+
+    Returns:
+        profit (RootResults): the return is profit
+
+    """ 
+    
+    # a. Objective function
+    if p_i > p_j: 
+        profit = 0
+    elif p_i == p_j: 
+        profit = (p_i-c)*1/2*demand
+    else:
+        profit = (p_i-c)*1/2*demand
+
+    return profit
+
